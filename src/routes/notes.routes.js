a963859e399ff64e100c548fe62ecb23020689a9
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2024-02-17 15:28:27 +0000

    init

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,16 @@
+const AppError = require("../utils/AppError");
+const { Router, json } = require("express");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
+const NotesController = require("../controllers/NotesController");
+const notesRoutes = Router();
+const notesController = new NotesController();
+
+notesRoutes.use(ensureAuthenticated);
+
+notesRoutes.get("/", notesController.index);
+notesRoutes.get("/:id", notesController.show);
+notesRoutes.post("/", notesController.create);
+notesRoutes.delete("/:id", notesController.delete);
+
+module.exports = notesRoutes;
